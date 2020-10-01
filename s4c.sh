2cddd5080211cf3da6a1f5138ba6a6930cbb7667
cosh <coshxx@googlemail.com>
2020-10-01 18:12:37 +0200

    Have outfile as a parameter

--- s4c.sh
+++ s4c.sh
@@ -6,15 +6,14 @@ PASSWORD=""
 URL=""
 SLEEP_TIME=28800 #8 hours
 SEARCH_STRING='This Page is unchanged'
+FILE_OUT=out.html
 
 for (( ; ; )); do
-    curl $URL -L --compressed -s > new.html
-    RESULT="$(cat new.html | grep "$SEARCH_STRING")"
+    curl $URL -L --compressed -s > $FILE_OUT
+    RESULT="$(cat $FILE_OUT | grep "$SEARCH_STRING")"
     if [ "0" != "${#RESULT}" ]; then
-        # Bar is still closed. Continue.
         sleep $SLEEP_TIME
     else
-        echo NO_CHANGE!!
         sendEmail -f $USERNAME -s smtp.gmail.com:587 \
             -xu $USERNAME -xp $PASSWORD -t $RECIPIENT \
             -o tls=yes -u "Web page changed" \
